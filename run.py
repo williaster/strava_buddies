1593f08c23e496c21b4931250dcfa4d268089a2e
Ubuntu <ubuntu@ip-172-31-20-76.us-west-1.compute.internal>
2014-09-22 07:32:55 +0000

    stravabuddies pre local pull

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
 #!/Users/christopherwilliams/dotfiles/virtualenvs/.virtualenvs/lighttable/bin/python
 from app import app
-app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=80)
+  
