8de0e392ac30492c85de82f312c93f5e575a1391
williaster <williaster@gmail.com>
2014-09-29 15:34:16 -0700

    add validation plots for three athletes

---
+++ R/nicole/validation_nicole.R
@@ -0,0 +1,54 @@
+library(ggplot2)
+orange <- "#f74f00"
+blue   <- "#167ebb"
+grey   <- "gray50"
+
+getSD <- function(y, n=1) {
+    y.mean <- mean(y)
+    y.sd   <- sd(y)
+    result <- c(mean(y) - sd(y), mean(y) + sd(y))
+    names(result) <- c("ymin", "ymax")
+    data.frame(y=y.mean, ymin=y.mean - (n*y.sd), ymax=y.mean + (n*y.sd))
+}
+
+isetwd("~/Dropbox/insight-data-science/project/strava_buddies/R/nicole")
+#load(".RData")
+f.bigbins           <- "validation_nicole_100x-bins.csv"
+f.random.sim        <- "random_similarities_nicole.csv"
+f.random.friend.sim <- "random_friend_similarities_nicole.csv"
+
+
+df.bigbins  <- read.csv(f.bigbins)
+df.stravabuddies <- df.bigbins[,c("median", "n_activities")]
+df.stravabuddies["algorithm"] <- "stravabuddies"
+
+df.random.sim <- read.csv(f.random.sim, header=FALSE, 
+                          col.names=c("trial", "median"))
+df.random.sim$n_activities <- sample(1:15, size=500, replace=TRUE)
+df.random <- df.random.sim[,c("median", "n_activities")]
+df.random["algorithm"] <- "random"
+
+
+df.random.friend.sim <- read.csv(f.random.friend.sim, header=FALSE, 
+                                 col.names=c("trial", "median"))
+df.random.friend.sim$n_activities <- sample(1:15, size=500, replace=TRUE)
+df.friends <- df.random.friend.sim[,c("median", "n_activities")]
+df.friends["algorithm"] <- "friends"
+
+
+df.final <- do.call(rbind, list(df.stravabuddies, df.friends, df.random))
+
+ggplot(df.final, aes(x=n_activities, y=median, color=algorithm, fill=algorithm)) +
+    geom_point(alpha=0.25) + 
+    scale_color_manual(values=c(blue, grey, orange)) +
+    scale_fill_manual(values=c(blue, grey, orange)) +
+    xlab("Number of activities considered") +
+    ylab("Median similarity") + 
+    stat_summary(fun.data = "getSD", geom="smooth") +
+    #geom_smooth(level=0.99) +
+    xlim(c(0,16)) + ylim(c(0.2, 0.7))
+
+
+pdf("validation_algs_nicole.pdf", width=5, height=5, useDingbats=FALSE)
+p
+dev.off()
--- R/validation.R
+++ R/validation.R
@@ -1,35 +1,100 @@
 library(ggplot2)
 
 setwd("~/Dropbox/insight-data-science/project/strava_buddies/R/")
-f.validation  <- "validation.csv"
-f.random.sim  <- "random_similarities.csv"
+load(".RData")
 
-df.validation <- read.csv(f.validation)
-df.random.sim <- read.csv(f.random.sim, header=FALSE, col.names=c("trial", "random_similarity"))
+#....................................................................................
+# variables and helper functions
+orange <- "#f74f00"
+blue   <- "#167ebb"
+grey   <- "gray50"
 
-df.random.sim$n_activities <- sample(1:15, size=500, replace=TRUE)
+get.sd <- function(y, n=1) {
+    y.mean <- mean(y)
+    y.sd   <- sd(y)
+    result <- c(mean(y) - sd(y), mean(y) + sd(y))
+    names(result) <- c("ymin", "ymax")
+    data.frame(y=y.mean, ymin=y.mean - (n*y.sd), ymax=y.mean + (n*y.sd))
+}
 
+get.random <- function(f, algorithm) {
+    df.f <- read.csv(f, header=FALSE, col.names=c("trial", "median"))
+    df   <- data.frame(median = df.f[,"median"],
+                       n_activities = sample(1:15, size=nrow(df.f), replace=TRUE),
+                       algorithm    = algorithm )
+    df
+}
 
-ggplot(df.validation, aes(x=miles, y=fract_below)) +
-    geom_point() +
-    theme_minimal() +
-    geom_hline(aes(yint=fract_below_random), color="red") 
-
-ggplot(df.validation, aes(x=n_activities, y=min)) +
-    geom_point() +
-    theme_minimal() +
-    geom_hline(aes(yint=min_random), color="red") 
+get.user.df <- function(f.stravabuddies, f.randomfriends, f.random, athlete) {   
+    df.stravabuddies <- read.csv(f.stravabuddies)[,c("median", "n_activities")]
+    df.stravabuddies["algorithm"] <- "stravabuddies"
+   
+    df.friends <- get.random(f.randomfriends, "friends")
+    df.random  <- get.random(f.random, "random")
     
+    df.final <- do.call(rbind, list(df.stravabuddies, df.friends, df.random))
+    df.final["athlete"] <- athlete
+    df.final
+}
 
-p <-    
-    ggplot(df.validation, aes(x=n_activities, y=median)) +
-    geom_point(color="#167ebb") +
-    geom_point(data=df.random.sim, aes(x=n_activities, y=random_similarity), color="gray50") +
+#....................................................................................
+# chris files
+f.c.bigbins           <- "chris/validation_chris_50x-bins.csv"
+f.c.random.sim        <- "chris/random_medians_chris.csv"
+f.c.random.friend.sim <- "chris/random_friend_medians_chris.csv"
+
+# rob files
+f.r.bigbins           <- "rob/validation_rob_50x-bins.csv"
+f.r.random.sim        <- "rob/random_medians_rob.csv"
+f.r.random.friend.sim <- "rob/random_friend_medians_rob.csv"
+
+# nicole files
+f.n.bigbins           <- "nicole/validation_nicole_50x-bins.csv"
+f.n.random.sim        <- "nicole/random_medians_nicole.csv"
+f.n.random.friend.sim <- "nicole/random_friend_medians_nicole.csv"
+
+#....................................................................................
+#
+
+df.chris  <- get.user.df(f.c.bigbins, f.c.random.friend.sim, f.c.random.sim, "Athlete 3")
+df.rob    <- get.user.df(f.r.bigbins, f.r.random.friend.sim, f.r.random.sim, "Athlete 2")
+df.nicole <- get.user.df(f.n.bigbins, f.n.random.friend.sim, f.n.random.sim, "Athlete 1")
+
+# rob has outliers
+df.rob   <- subset(df.rob, median >= 0.275)
+df.final <- do.call(rbind, list(df.chris, df.rob, df.nicole))
+
+p <- ggplot(df.final, aes(x=n_activities, y=median, color=algorithm, fill=algorithm)) +
+    geom_point(alpha=0.25) + 
+    scale_color_manual(values=c(blue, grey, orange)) +
+    scale_fill_manual(values=c(blue, grey, orange)) +
     xlab("Number of activities considered") +
-    ylab("Median similarity") +
-    xlim(c(0,16)) + ylim(c(0, 0.6))
+    ylab("Median similarity") + 
+    stat_summary(fun.data = "get.sd", geom="smooth") +
+    xlim(c(0,16)) + #ylim(c(0.2, 0.7)) +
+    facet_wrap(~athlete, ncol=3, scales="free_y") +
+    #geom_vline(xint=2, linetype="dashed") +
+    theme_bw() +
+    theme(legend.position="none")
 
-pdf("median_similarity_vs_random.pdf", width=5, height=5, useDingbats=FALSE)
+
+p.nofriends <- ggplot(subset(df.final, algorithm %in% c("random", "stravabuddies")), 
+                      aes(x=n_activities, y=median, color=algorithm, fill=algorithm)) +
+    geom_point(alpha=0.25) + 
+    scale_color_manual(values=c(grey, orange)) +
+    scale_fill_manual(values=c(grey, orange)) +
+    xlab("Number of activities considered") +
+    ylab("Median similarity") + 
+    stat_summary(fun.data = "get.sd", geom="smooth") +
+    xlim(c(0,16)) + #ylim(c(0.2, 0.7)) +
+    facet_wrap(~athlete, ncol=3, scales="free_y") +
+    theme_bw() +
+    theme(legend.position="none")
+
+pdf("median_similarity_vs_random_vs_friends_all.pdf", width=11, height=3.5, useDingbats=FALSE)
 p
 dev.off()
 
+pdf("median_similarity_vs_random_all.pdf", width=11, height=3.5, useDingbats=FALSE)
+p.nofriends
+dev.off()
